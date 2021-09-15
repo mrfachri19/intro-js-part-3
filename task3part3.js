058d9c84e6cf6d1235453708df2c17076cccd2fe
fachri <83835862+mrfachri19@users.noreply.github.com>
2021-09-15 12:07:00 +0700

    Update task3part3.js
    finish

--- task3part3.js
+++ task3part3.js
@@ -1,6 +1,6 @@
 // program 1
 let dataLoad = true;
- const loadingData = new Promise ((resolve, reject) =>{
+ const loadingData = new Promise ((resolve) =>{
    if (dataLoad) {
      setTimeout(() =>{
        resolve(' loading selesai');
@@ -13,26 +13,27 @@ let dataLoad = true;
  });
 console.log('mulai');
 loadingData
-  .finally(() => console.log('selesai menunggu'))
+  
   .then(response => console.log(response))
-  .catch(response => console.log( response));
+  .catch(response => console.log( response))
+  .finally(() => console.log('selesai menunggu'));
   console.log('selesai');
 
-  // program2
-  let makan = "kenyang"
+//   // program2
+//   let makan = "kenyang"
 
-let p = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        if (makan === "kenyang") {
-            resolve("kamu sudah makan")
-        }  else {
-            reject("kamu belum makan")
-        }
-    }, 2000)
-})
+// let p = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         if (makan === "kenyang") {
+//             resolve("kamu sudah makan")
+//         }  else {
+//             reject("kamu belum makan")
+//         }
+//     }, 2000)
+// })
 
-p.then((result) => {
-    console.log(result)
-}).catch((error) => {
-    console.log(error)
-})
+// p.then((result) => {
+//     console.log(result)
+// }).catch((error) => {
+//     console.log(error)
+// })
